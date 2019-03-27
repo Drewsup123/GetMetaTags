05b6933dcf96f1522838893ccb23d02224b5f787
Drew <drewsup123c@gmail.com>
2019-03-27 11:39:56 -0600

    looks as though it is returning correctly

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ server.post('/',(req,res) => {
     res.status(200).send("hello")
 })
 
-server.get('/',(req,res) => {
+server.post('/get-meta',(req,res) => {
     const data = req.body;
-    res.status(200).send("hello")
+    urlMetadata('https://medium.freecodecamp.org/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3')
+    .then(data => {
+        res.status(200).send(data)
+    })
+    .catch(err=>{
+        res.status(500).send(err)
+    })
 })
 
 // urlMetadata('http://bit.ly/2ePIrDy').then(
