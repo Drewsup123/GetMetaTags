6ca54b756578e037a1f4726c437589c187a587bf
Drew <drewsup123c@gmail.com>
2019-03-27 12:09:39 -0600

    should work as needed

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require("express");
-const cors = require("cors");
 const server = express();
+const cors = require("cors");
 const urlMetadata = require('url-metadata')
 
-server.post('/',(req,res) => {
-    const data = req.body;
+server.use(express.json());
+server.use(cors());
+
+server.get('/',(req,res) => {
     res.status(200).send("hello")
 })
 
-server.post('/get-meta',(req,res) => {
-    const data = req.body;
-    urlMetadata('https://medium.freecodecamp.org/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3')
-    .then(data => {
-        res.status(200).send(data)
-    })
-    .catch(err=>{
-        res.status(500).send(err)
-    })
+server.post('/get-meta', (req, res) => {
+    const url = req.body.url;
+    if(url){
+        if(url.includes("http://") || url.includes("https://")){
+            urlMetadata(url)
+            .then(data => {
+                res.status(200).send(data)
+            })
+            .catch(err=>{
+                res.status(500).send(err)
+            })
+        }else{
+            res.status(400).send("bad request. Make sure to have http or https in req.body")
+        }
+
+    }else{
+        res.status(500).send("Url not provided");
+    }
 })
 
-// urlMetadata('http://bit.ly/2ePIrDy').then(
-//   function (metadata) { // success handler
-//     console.log(metadata)
-//   },
-//   function (error) { // failure handler
-//     console.log(error)
-//   })
 module.exports = server;
