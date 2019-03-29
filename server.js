b029b24b1226b699b24fd5f49c1860ba349ea77e
Hunter315 <hunter.mckay.smith@gmail.com>
2019-03-28 23:00:45 -0600

    axios get within post request on udemy- testing on deploy, localhost failed

--- server.js
+++ server.js
@@ -1,50 +1,59 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-const urlMetadata = require('url-metadata');
+const urlMetadata = require("url-metadata");
 const cheerio = require("cheerio");
-const request = require('request');
+const axios = require("axios");
+
 server.use(express.json());
 server.use(cors());
 
-server.get('/',(req,res) => {
-    res.status(200).send("hello")
-})
 
-server.post('/get-meta', (req, res) => {
-    const url = req.body.url;
-    if(url){
-        if(url.includes("http://") || url.includes("https://")){
-            urlMetadata(url)
-            .then(data => {
-                res.status(200).send(data)
-            })
-            .catch(err=>{
-                res.status(500).send(err)
-            })
-        }else{
-            res.status(400).send("bad request. Make sure to have http or https in req.body")
-        }
+server.get("/", (req, res) => {
+  res.status(200).send("hello");
+});
 
-    }else{
-        res.status(500).send("Url not provided");
+server.post("/get-meta", (req, res) => {
+  const url = req.body.url;
+  if (url) {
+    if (url.includes("http://") || url.includes("https://")) {
+      urlMetadata(url)
+        .then(data => {
+          res.status(200).send(data);
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        });
+    } else {
+      res
+        .status(400)
+        .send("bad request. Make sure to have http or https in req.body");
     }
-})
+  } else {
+    res.status(500).send("Url not provided");
+  }
+});
 
-server.get('/user-udemy', (req, res) => {
-    request(req, (err, res, body) => {
-//Load HTML body into cheerio
-console.log(body)
-//Cheerio functions
-})
-    // const url = req.body.url;
-    // if(url){
-    //     const data = cheerio.load(url)
+server.post("/user-udemy", (req, res) => {
 
-    //     res.status(200).send(data)
+  console.log(req.body.url);
+
+  axios.get(req.body.url).then((response) => {
+      console.log(response.data)
+    return response.data
+  }).catch((err)=> {
+    console.log(err)
+  })
 
 
-    // }
-})
+  
+  // const url = req.body.url;
+  // if(url){
+  //     const data = cheerio.load(url)
+
+  //     res.status(200).send(data)
+
+  // }
+});
 
 module.exports = server;
