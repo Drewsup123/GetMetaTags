e0d059f7a394704e028968d77e0a3ae7824bc3e3
Hunter315 <hunter.mckay.smith@gmail.com>
2019-04-02 15:03:33 -0600

    udemy cat addintion

--- server.js
+++ server.js
@@ -2,15 +2,35 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 const urlMetadata = require("url-metadata");
-const request = require("request")
-const cheerio = require('cheerio');
+const request = require("request");
+const cheerio = require("cheerio");
 const requestPromise = require("request-promise");
 server.use(express.json());
 server.use(cors());
 
-
 server.get("/", (req, res) => {
   res.status(200).send("hello");
+});
+
+server.post("/udemy-cat", async (req, res) => {
+  console.log(req.body);
+  let category = req.body;
+  await requestPromise(
+    {
+      method: "GET",
+      url: "https://www.udemy.com/api-2.0/courses/?category=" + category,
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        Authorization:
+          "Basic eFAxZ0FSblRtVTJyYWlvZ1FGQ2JqQXE1dWZhRXRuanpEMWJQMkxBOTpSN21HQ3lrZUdjaVJEbTc0UXNtaGZDMEdzUG1oWjVRTUhEaHlCTGo2UmxvNFJ2dmQ3aWJnYU0ycjZmQWR0S0tlYXJRZFJvNEpPR3JGNEJZRG1TSnRBdVpheUt3Ykw3alRsV1J3NkkxSklFSkM0RVNDSXM3WDUzNlVtRVB1VlA4MA==",
+        "Content-Type": "application/json;charset=utf-8"
+      }
+    },
+    function(error, response, body) {
+      console.log(body)
+      res.status(200).send(body)
+    }
+  );
 });
 
 server.post("/get-meta", (req, res) => {
@@ -34,7 +54,8 @@ server.post("/get-meta", (req, res) => {
   }
 });
 
-server.post("/user-udemy", async (req, res) => { //RUNTIME == 45-55 seconds
+server.post("/user-udemy", async (req, res) => {
+  //RUNTIME == 45-55 seconds
   // api key :  9bd569c5901a72fa4a94d2b525a9b007
   var url = req.body.url; //this will be dynamic
   let links = []; // original array of "/coursename" links
@@ -42,45 +63,47 @@ server.post("/user-udemy", async (req, res) => { //RUNTIME == 45-55 seconds
   let Final = []; //Final array full of metaData from href endpoints
 
   await requestPromise(
-      {
-        method: 'GET',
-        url: 'http://api.scraperapi.com/?key=9bd569c5901a72fa4a94d2b525a9b007&url=' + url + '&render=true',
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-      function(error, response, body) {
-          const $ = cheerio.load(body); // loads the html body, which should be a string of html content
-          $('.merchandising-course-card--mask--2-b-d').each(function(i, elm) { //go to each class of the <a> of courses. In this case it is '.merchandising....' class
-            links.push($(elm).attr('href')); // push only the href of each element to links array
-          });
-          for(let i = 0; i < links.length; i++){
-            LinksArr.push(`https://www.udemy.com${links[i]}`) // pushes full link to Final array so we can reference it later
-          }
+    {
+      method: "GET",
+      url:
+        "http://api.scraperapi.com/?key=9bd569c5901a72fa4a94d2b525a9b007&url=" +
+        url +
+        "&render=true",
+      headers: {
+        Accept: "application/json"
       }
+    },
+    function(error, response, body) {
+      const $ = cheerio.load(body); // loads the html body, which should be a string of html content
+      $(".merchandising-course-card--mask--2-b-d").each(function(i, elm) {
+        //go to each class of the <a> of courses. In this case it is '.merchandising....' class
+        links.push($(elm).attr("href")); // push only the href of each element to links array
+      });
+      for (let i = 0; i < links.length; i++) {
+        LinksArr.push(`https://www.udemy.com${links[i]}`); // pushes full link to Final array so we can reference it later
+      }
+    }
   ); //end request()
-  
+
   //need to wait for this function to run before sending the res
-  await (async ()=> {
-    for(let i = 0; i < LinksArr.length; i++){
+  await (async () => {
+    for (let i = 0; i < LinksArr.length; i++) {
       console.log(LinksArr[i]);
       // need to wait for this function as well
       await urlMetadata(LinksArr[i])
         .then(data => {
-          Final.push({...data})
-          console.log(Final)
+          Final.push({ ...data });
+          console.log(Final);
         })
         .catch(err => {
-          return -1
+          return -1;
         });
       // console.log("METADATA", metaData)
       // Final.push({...metaData, url : LinksArr[i]})
       // console.log("final at ",i,Final)
     }
   })();
-    res.status(200).send(Final);
+  res.status(200).send(Final);
 });
 
 module.exports = server;
-
-
