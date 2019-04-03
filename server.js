a82b72b349bbb8156d16d04b2bb6886705b6ecd9
Hunter315 <hunter.mckay.smith@gmail.com>
2019-04-03 10:10:48 -0600

    take out cors options

--- server.js
+++ server.js
@@ -53,7 +53,9 @@ var corsOptions = {
   }
 }
 
-server.post("/udemy-cat", cors(corsOptions), async (req, res) => {
+server.post("/udemy-cat", 
+// cors(corsOptions),
+ async (req, res) => {
  
   console.log(req.body.category);
   let categoryArr = req.body
